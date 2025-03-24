3aad23e3f17e5bf0ef100468d5b4c3b6ff3a89b1
Yasir Ali <sdr.yasir@gmail.com>
2025-03-24 02:27:15 -0700

    props

--- src/App.jsx
+++ src/App.jsx
@@ -5,78 +5,9 @@ import './App.css'
 
 function App() {
 
-  const [todo, setTodo] = useState({
-    title:'',
-    description:'',
-    isCompleted:false,
-    isImportant:false,
-    dueDate:''
-  });
-
-  const [todos, setTodos] = useState([]);
-
-  const handleTitle = (e)=>{    
-    setTodo({...todo, title:e.target.value});
-  }
-
-  const handleDescription = (e)=>{
-    setTodo({...todo, description:e.target.value})
-  }
-  const handleSubmit = (e)=>{
-    e.preventDefault();
-
-    let newTodo = {
-      ...todo,
-      id:Date.now()
-    }
-
-    let a = [...todos, newTodo]
-    
-    setTodos(a);
-
-  }
-
-
-  let handleComplete = (t)=>{
-
-    const foundIndex = todos.findIndex(item => item.id == t.id);
-
-    const updatedArray = [...todos]; // Copy the original array
-
-    updatedArray[foundIndex] = { ...updatedArray[foundIndex], isCompleted:!updatedArray[foundIndex].isCompleted };
-
-    setTodos(updatedArray);
-
-  }
   return (
     <div className="container">
-      <div className="todo-wrapper bg-info p-5 w-50">
-        <form onSubmit={handleSubmit}>
-          <div className="row mb-3">
-            <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Title</label>
-            <div className="col-sm-10">
-              <input type="text" onChange={handleTitle} className="form-control" id="inputEmail3" />
-            </div>
-          </div>
-          <div className="row mb-3">
-            <label htmlFor="description" className="col-sm-2 col-form-label">Description</label>
-            <div className="col-sm-10">
-              <textarea className="form-control" onChange={handleDescription} name="" id=""></textarea>
-            </div>
-          </div>
-          <button type="submit" className="btn btn-primary">Save Todo</button>
-        </form>
-        {
-          todos.map((todo)=>{
-            return <div key={todo.id} style={{backgroundColor:todo.isCompleted?'red':'green', padding:'6px', margin:'6px'}}>
-              <h1 style={{textDecoration: todo.isCompleted ? 'line-through': 'none'}} onClick={()=>handleComplete(todo)}>{todo.title}</h1>
-              {
-                todo.isCompleted ? <span>Completed</span>:<span>Not completed</span>
-              }
-            </div>
-          })
-        }
-      </div>
+      <p>Hello Props</p>
     </div>
   )
 
