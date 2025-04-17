1c6608e691e4393c48d1221c837174583eab2745
Yasir Ali <sdr.yasir@gmail.com>
2025-04-17 04:17:24 -0700

    cart

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,16 +13,38 @@ function Home() {
     };
 
 
-
-
-
-
-
-
       const handleCart = (prod)=>{
         cartContext.setCart([...cartContext.cart, prod]);
-        
       }
+
+      const handleIncrement = (id)=>{
+        const obj = cartContext.cart.find((item)=>item.id == id);
+      
+        const index = cartContext.cart.findIndex((itm)=>itm.id == id);
+      
+        const updatedObj = {...obj, qty: obj.qty + 1};
+      
+        cartContext.cart[index] = updatedObj;
+      
+        cartContext.setCart([...cartContext.cart]);
+       }
+
+
+       const handleDecrement = (id)=>{
+        const obj = cartContext.cart.find((item)=>item.id == id);
+       
+         const index = cartContext.cart.findIndex((itm)=>itm.id == id);
+
+         if(obj.qty<=1){
+            return;
+           }
+       
+         const updatedObj = {...obj, qty: obj.qty - 1};
+       
+         cartContext.cart[index] = updatedObj;
+       
+         cartContext.setCart([...cartContext.cart]);
+        }
 
     return (
        <>
@@ -39,15 +61,21 @@ function Home() {
                                         <Link to={`/products/${product.id}`}><h5 className="card-title">{product.title}</h5></Link>
                                         <p className="card-text">{truncate(product.description, 50)}</p>
                                         <a href="#" className="btn btn-primary me-3">$ {product.price}</a>
-                                        <button className="btn btn-primary" disabled={cartContext.cart.find((p)=>p.id == product.id) ? true : false} onClick={()=>handleCart(product)}>Add to Cart</button>
+                                            {
+                                                cartContext.cart.find((p)=>p.id == product.id) ? 
+                                                <>
+                                                    <button onClick={()=>handleDecrement(cartContext.cart.find((p)=>p.id == product.id).id)}>-</button>
+                                                    <span>{ cartContext.cart.find((p)=>p.id == product.id).qty }</span>
+                                                    <button onClick={()=>handleIncrement(cartContext.cart.find((p)=>p.id == product.id).id)}>+</button>
+                                                </>
+                                                :<button className="btn btn-primary" onClick={()=>handleCart(product)}>Add to Cart</button>
+                                            }
                                     </div>
                                 </div>
                             </div>
                         )
                     })
                 }
-
-
             </div>
         </div>
        </>
